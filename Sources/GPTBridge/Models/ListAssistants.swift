9776e9811e1ea2517f63391e54d1dc0e57ad20ed
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-03-25 10:25:21 -0700

    added public init for Assistant

--- Sources/GPTBridge/Models/ListAssistants.swift
+++ Sources/GPTBridge/Models/ListAssistants.swift
@@ -17,5 +17,13 @@ public struct Assistant: Codable, Equatable {
     public let name: String
     public let description: String?
     public let model: String
-    public let instructions: String
+    public let instructions: String?
+
+    public init(id: String, name: String, description: String?, model: String, instructions: String?) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.model = model
+        self.instructions = instructions
+    }
 }
