ad2e65641da61b04cf0b042a6038f58a8cffe9e4
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-04-05 22:58:26 -0700

    implemented list assistants in embedded project

---
+++ Sources/My GPT Dashboard/My GPT Dashboard/My_GPT_DashboardApp.swift
@@ -0,0 +1,35 @@
+//
+//  My_GPT_DashboardApp.swift
+//  My GPT Dashboard
+//
+//  Created by Kenneth Dubroff on 4/5/24.
+//
+
+import GPTBridge
+import SwiftUI
+
+@main
+struct My_GPT_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                AssistantListView()
+                    .tabItem {
+                        Label("Assistants", systemImage: "person.2.badge.gearshape.fill")
+                    }
+            }
+            .task {
+                GPTBridge.appLaunch(openAIAPIKey: "my api key (sk-xxx)", assistantKey: "my assistant key (asst_xxxx")
+            }
+        }
+    }
+}
+
+#Preview {
+    TabView {
+        AssistantListView()
+            .tabItem {
+                Text("Assistants")
+            }
+    }
+}
