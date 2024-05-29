5601465bbc2542af6e19e94fac05de79adfc6003
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-05-28 20:49:22 -0700

    added `CreateRunTests`

--- Tests/GPTBridgeTests/CreateRunTests.swift
+++ Tests/GPTBridgeTests/CreateRunTests.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
-//  
+//  CreateRunTests.swift
+//
 //
 //  Created by Kenneth Dubroff on 5/28/24.
 //
 
-import Foundation
+import XCTest
+@testable import GPTBridge
+
+class CreateRunTests: XCTestCase {
+    func testCanDecodeRunObject() throws {
+        let instance = try XCTUnwrap(
+            toInstance(
+                from: runThreadResponseJSONString,
+                to: RunThreadResponse.self
+            )
+        )
+        XCTAssertEqual(instance.id, testRunThreadResponse.id)
+    }
+}
