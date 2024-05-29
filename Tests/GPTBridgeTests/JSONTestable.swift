855acfa014c5d646d5c2ba132c5913b682c325ab
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-05-28 18:33:50 -0700

    removed `toJSONString` in order to test actual response JSON

--- Tests/GPTBridgeTests/JSONTestable.swift
+++ Tests/GPTBridgeTests/JSONTestable.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 protocol JSONTestable {
-    func toJSONString(from instance: Any, file: StaticString, line: UInt) throws -> String
     func toInstance<T: Decodable>(from jsonString: String, to targetType: T.Type, usingKeyDecodingStrategy keyStrategy: JSONDecoder.KeyDecodingStrategy, file: StaticString, line: UInt) throws -> T
 }
 
@@ -35,85 +34,6 @@ extension JSONTestable {
             throw error
         }
     }
-
-    private func toSnakeCaseDictionary(_ instance: Any) -> [String: Any] {
-        let mirror = Mirror(reflecting: instance)
-        var dict = [String: Any]()
-        for child in mirror.children {
-            if let key = child.label {
-                var convertedKey = ""
-                for char in key {
-                    if char.isUppercase {
-                        convertedKey.append("_" + char.lowercased())
-                    } else {
-                        convertedKey.append(char)
-                    }
-                }
-                dict[convertedKey] = child.value
-            }
-        }
-        return dict
-    }
-
-    func toJSONString(from instance: Any, file: StaticString = #file, line: UInt = #line) throws -> String {
-        var dict = toSnakeCaseDictionary(instance)
-        var data: Data
-        if JSONSerialization.isValidJSONObject(dict) {
-            data = try JSONSerialization.data(withJSONObject: dict, options: [])
-        } else {
-            let mirror = Mirror(reflecting: dict)
-            dict = [:]
-            var returnKey = ""
-
-            for child in mirror.children {
-                // value should be a tuple due to prior conversion to dictionary
-                if let valueDict = child.value as? (String, Any) {
-                    returnKey = valueDict.0
-                    // this is a Swift type and fails serialization
-                    let swiftTypeInstance = valueDict.1
-                    let valueMirror = Mirror(reflecting: swiftTypeInstance)
-                    var propertyDict: [String: Any] = [:]
-
-                    for child in valueMirror.children {
-                        // property is directly serializable, add to propertyDict
-                        if let key = child.label,
-                           JSONSerialization.isValidJSONObject([key: child.value]) {
-                            propertyDict[key] = child.value
-                        } else {
-                            // property is not directly serializable, maybe another Swift type
-                            let swiftObjectJSONMirror = Mirror(reflecting: child.value)
-                            // put each property in a dictionary
-                            for child in swiftObjectJSONMirror.children {
-                                if let key = child.label {
-                                    // make sure data is still serializable
-                                    if JSONSerialization.isValidJSONObject([key: child.value]) {
-                                        propertyDict[key] = child.value
-                                    }
-                                }
-                            }
-                        }
-                        // Note: Assumes property is an array
-                        if dict[returnKey] != nil,
-                           var array = dict[returnKey] as? [Any] {
-                            array.append(propertyDict)
-                            dict[returnKey] = array
-                        } else {
-                            dict[returnKey] = [propertyDict]
-                        }
-                        propertyDict = [:]
-                    }
-                }
-            }
-
-            data = try JSONSerialization.data(withJSONObject: dict, options: [])
-
-        }
-        guard let jsonString = String(data: data, encoding: .utf8) else {
-            XCTFail("The instance couldn't be converted to data", file: file, line: line)
-            throw JSONError.invalidJSONData
-        }
-        return jsonString
-    }
 }
 
 extension XCTestCase: JSONTestable {}
@@ -133,18 +53,9 @@ class JSONTestAbleTests: XCTestCase {
     let validJSON = """
                     { "a_string":"test","num":1 }
                     """
-    let invalidJSON = """
-                    "num": 1,"a_string": "test"
-                    """
 
     func testToJSONData() throws {
         let testInstance = try toInstance(from: validJSON, to: JSONTest.self)
         XCTAssertEqual(testInstance, self.testInstance)
-    }
-
-    func testToJSONString() throws {
-        let string = try toJSONString(from: testInstance)
-        XCTAssert(string.contains("\"a_string\":\"test\""))
-        XCTAssert(string.contains("\"num\":1"))
     }
 }
--- Tests/GPTBridgeTests/ListAssistantsTests.swift
+++ Tests/GPTBridgeTests/ListAssistantsTests.swift
@@ -9,21 +9,81 @@ import XCTest
 @testable import GPTBridge
 
 class ListAssistantsTests: XCTestCase {
-    let testAssistant = Assistant(id: "id", name: "name", description: "description", model: "model", instructions: "instructions")
-    private func assistantJSONString() throws -> String {
-        try toJSONString(from: testAssistant)
+    private var testAssistant1: Assistant {
+        Assistant(id: "asst_abc123", name: "Coding Tutor", description: nil, model: "gpt-4-turbo", instructions: "You are a helpful assistant designed to make me better at coding!")
+    }
+
+    private var testAssistant2: Assistant {
+        Assistant(id: "asst_abc456", name: "My Assistant", description: nil, model: "gpt-4-turbo", instructions: "You are a helpful assistant designed to make me better at coding!")
+    }
+
+    private var assistantJSONString: String {
+        """
+        {
+          "id": "\(testAssistant1.id)",
+          "name": "\(testAssistant1.name)",
+          "description": null,
+          "model": "\(testAssistant1.model)",
+          "instructions": "\(testAssistant1.instructions!)"
+        }
+        """
+    }
+
+    var testResponse: ListAssistantsResponse {
+        ListAssistantsResponse(data: [testAssistant1, testAssistant1])
+    }
+
+    private var listAssistantsReponseJSONString: String {
+        """
+        {
+          "object": "list",
+          "data": [
+            {
+              "id": "asst_abc123",
+              "object": "assistant",
+              "created_at": 1698982736,
+              "name": "Coding Tutor",
+              "description": null,
+              "model": "gpt-4-turbo",
+              "instructions": "You are a helpful assistant designed to make me better at coding!",
+              "tools": [],
+              "tool_resources": {},
+              "metadata": {},
+              "top_p": 1.0,
+              "temperature": 1.0,
+              "response_format": "auto"
+            },
+            {
+              "id": "asst_abc456",
+              "object": "assistant",
+              "created_at": 1698982718,
+              "name": "My Assistant",
+              "description": null,
+              "model": "gpt-4-turbo",
+              "instructions": "You are a helpful assistant designed to make me better at coding!",
+              "tools": [],
+              "tool_resources": {},
+              "metadata": {},
+              "top_p": 1.0,
+              "temperature": 1.0,
+              "response_format": "auto"
+            }
+          ],
+          "first_id": "asst_abc123",
+          "last_id": "asst_abc789",
+          "has_more": false
+        }
+        """
     }
 
     func testAssistants_areDecodable() throws {
-        let decodedAssistant = try toInstance(from: assistantJSONString(), to: Assistant.self)
-        XCTAssertEqual(decodedAssistant, testAssistant)
+        let decodedAssistant = try toInstance(from: assistantJSONString, to: Assistant.self)
+        XCTAssertEqual(decodedAssistant, testAssistant1)
     }
 
     func testAssistantsResponse_canBeDecoded() throws {
-        let testResponse = ListAssistantsResponse(data: [testAssistant, testAssistant])
-        let responseJSONString = try toJSONString(from: testResponse)
-        let decodedResponse = try toInstance(from: responseJSONString, to: ListAssistantsResponse.self)
-        XCTAssertEqual(decodedResponse.data[0], testAssistant)
-        XCTAssertEqual(decodedResponse.data[1], testAssistant)
+        let decodedResponse = try toInstance(from: listAssistantsReponseJSONString, to: ListAssistantsResponse.self)
+        XCTAssertEqual(decodedResponse.data[0], testAssistant1)
+        XCTAssertEqual(decodedResponse.data[1], testAssistant2)
     }
 }
