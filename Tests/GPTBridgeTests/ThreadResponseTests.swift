e1b27337fe80dbd3c1b19c6f728b9397bf71a185
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-05-28 19:56:58 -0700

    added ThreadResponse tests

--- Tests/GPTBridgeTests/ThreadResponseTests.swift
+++ Tests/GPTBridgeTests/ThreadResponseTests.swift
@@ -1,8 +1,41 @@
 //
-//  File.swift
-//  
+//  ThreadResponseTests.swift
+//
 //
 //  Created by Kenneth Dubroff on 5/28/24.
 //
 
-import Foundation
+import XCTest
+@testable import GPTBridge
+
+class ThreadResponseTests: XCTestCase {
+    private var testThreadResponse: CreateThreadResponse {
+        CreateThreadResponse(id: "thread_abc123")
+    }
+
+    private var testThreadResponseJSONString: String {
+        """
+          {
+            "id": "thread_abc123",
+            "object": "thread",
+            "created_at": 1699012949,
+            "metadata": {},
+            "tool_resources": {}
+          }
+        """
+    }
+
+    func testCanDecode_threadResponse() throws {
+        let testInstance = try XCTUnwrap(
+            toInstance(
+                from: testThreadResponseJSONString,
+                to: CreateThreadResponse.self
+            )
+        )
+
+        print(testInstance.id)
+
+        XCTAssertEqual(testInstance.id, testThreadResponse.id)
+    }
+
+}
