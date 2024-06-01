e199872fa7f84c8f00b28209a2017987f03fc7ab
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2024-06-01 13:28:19 -0700

    documentation

--- Sources/GPTBridge/Models/ListAssistants.swift
+++ Sources/GPTBridge/Models/ListAssistants.swift
@@ -12,18 +12,22 @@ public enum PaginationOrder: String, Encodable {
     case createdAt = "created_at"
     case descending = "desc"
 }
-
+/// Parameters for paginated requests
 public struct PaginatedRequestParameters {
+    /// A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 20.
     private(set) var limit: Int? = nil
-    private(set) var startAfter: String? = nil
-    private(set) var startBefore: String? = nil
+    /// Sort order by the created_at timestamp of the objects. asc for ascending order and desc for descending order.
     private(set) var order: PaginationOrder = .createdAt
-
+    /// A cursor for use in pagination. after is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include after=obj_foo in order to fetch the next page of the list.
+    private(set) var startAfter: String? = nil
+    /// A cursor for use in pagination. before is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include before=obj_foo in order to fetch the previous page of the list.
+    private(set) var startBefore: String? = nil
+    /// fetch the next page after the ID in `startAfter`
     public init(limit: Int, startAfter: String, order: PaginationOrder = .createdAt) {
         sharedInit(limit: limit, order: order)
         self.startAfter = startAfter
     }
-
+    /// fetch the previous page before the ID in `startBefore`
     public init(limit: Int, startBefore: String, order: PaginationOrder = .createdAt) {
         sharedInit(limit: limit, order: order)
         self.startBefore = startBefore
