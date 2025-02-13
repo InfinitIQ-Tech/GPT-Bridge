c1ebadab6aaa7add099efc4ab78c1631503c9f92
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2025-02-12 19:25:00 -0800

    updated ListAssistantsResponse - removed unecessary `ListAssistantsRequest`

--- Sources/GPTBridge/Models/ListAssistants.swift
+++ Sources/GPTBridge/Models/ListAssistants.swift
@@ -58,58 +58,23 @@ public struct PaginatedRequestParameters {
     }
 }
 
-/// Use this request to list assistants and paginate
-/// - NOTE: Leave all options nil if pagination is not required
-/// # Usage Example
-/// ```swift
-/// let request = ListAssistantsRequest(limit: 1)
-/// // get the response here <placeholder>
-/// if response.hasMore {
-///     let lastId = response.lastId
-///     // make the next paginated request
-///     let request = ListAssistantsRequest(after: lastId, limit: 1)
-/// }
-/// ```
-struct ListAssistantsRequest: EncodableRequest {
-    /// A cursor for use in pagination. after is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include after=obj_foo in order to fetch the next page of the list.
-    let after: String?
-    /// A cursor for use in pagination. before is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include before=obj_foo in order to fetch the previous page of the list.
-    let before: String?
-    /// A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 20.
-    let limit: Int?
-    /// Sort order by the created_at timestamp of the objects. asc for ascending order and desc for descending order.
-    let order: PaginationOrder?
-
-    init(
-        after: String? = nil,
-        before: String? = nil,
-        limit: Int? = nil,
-        order: PaginationOrder? = nil
-    ) {
-        self.after = after
-        self.before = before
-        self.limit = limit
-        self.order = order
-    }
-}
-
 public struct ListAssistantsResponse: DecodableResponse {
-    public let firstId: String
-    public let lastId: String
-    public let hasMore: Bool
+    public let firstId: String?
+    public let lastId: String?
+    public let hasMore: Bool?
     public let data: [Assistant]
 }
 
 public struct Assistant: Codable, Equatable {
     public let id: String
-    public let name: String
+    public let name: String?
     public let description: String?
     public let model: String
     public let instructions: String?
 
     public init(
         id: String,
-        name: String,
+        name: String?,
         description: String?,
         model: String,
         instructions: String?
